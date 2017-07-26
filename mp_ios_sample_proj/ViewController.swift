af1ca64bda6a1e91f0d82c2b23de9482b584dc34
Michael Pettiford <mpettiford@mpettiford-m01.ad.savagebeast.com>
2017-07-26 16:30:45 -0700

    commit

--- mp_ios_sample_proj/ViewController.swift
+++ mp_ios_sample_proj/ViewController.swift
@@ -10,12 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var questionLabel: UILabel!
-    @IBOutlet var answerLabel: UILabel!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,12 +20,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func showNextQuestion(_ sender: UIButton) {
-        
-    }
-    @IBAction func showAnswer(_ sender: UIButton) {
-        
-    }
 
 
 }
