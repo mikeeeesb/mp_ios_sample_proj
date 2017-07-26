7594687cdab8a393062d83eeec9b05732a2f017f
mike <geek@mikes-MacBook-Pro.local>
2017-07-25 21:24:37 -0700

    local

---
+++ mp_ios_sample_proj/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  mp_ios_sample_proj
+//
+//  Created by mike on 7/25/17.
+//  Copyright © 2017 mp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
+
+
+}
+
