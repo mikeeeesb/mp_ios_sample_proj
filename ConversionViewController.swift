abdea1621ec132e743bce65e22442eef21329161
Michael Pettiford <mpettiford@mpettiford-m01.ad.savagebeast.com>
2017-07-27 16:52:18 -0700

    commit

---
+++ ConversionViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ConversionViewController.swift
+//  mp_ios_sample_proj
+//
+//  Created by Michael Pettiford on 7/27/17.
+//  Copyright © 2017 mp. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController : UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+          updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+        
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+
+}
