ea3f1af3c452b22c960865155756c0fa24bedb1f
Michael Pettiford <mpettiford@pandora.com>
2017-09-08 14:28:12 -0700

    commit

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Michael Pettiford on 9/8/17.
+//  Copyright © 2017 Michael Pettiford. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController : UIViewController {
+    @IBOutlet var imageView : UIImageView!
+}
