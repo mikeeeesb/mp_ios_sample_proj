58d4b8e6ad3bf9e580ea80e1df6584e5ac3aa089
= <mikeesb@gmail.com>
2017-10-03 15:30:38 -0700

    commit

---
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Michael Pettiford on 9/26/17.
+//  Copyright © 2017 Michael Pettiford. All rights reserved.
+//
+
+import UIKit
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        update(with: nil)
+    }
+}
---
+++ Photorama/Photorama/PhotoDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Michael Pettiford on 9/26/17.
+//  Copyright © 2017 Michael Pettiford. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        return cell
+        
+    }
+}
